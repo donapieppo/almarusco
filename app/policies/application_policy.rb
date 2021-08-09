e3ec30dcdd79a9671e41b05d5aff80c9b56fee89
Pietro <donapieppo@yahoo.it>
2021-08-09 09:23:38 +0200

    current_organization_disposer?

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -3,6 +3,10 @@ class ApplicationPolicy < DmUniboCommon::ApplicationPolicy
     @user && OrganizationPolicy.new(@user, @user.current_organization).read?
   end
 
+  def current_organization_disposer?
+    @user && OrganizationPolicy.new(@user, @user.current_organization).dispose?
+  end
+
   def current_organization_manager?
     @user && OrganizationPolicy.new(@user, @user.current_organization).manage?
   end
