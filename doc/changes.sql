e445b71d92375372222fe554f1f4cd16010593d7
Pietro <donapieppo@yahoo.it>
2021-08-30 16:26:17 +0200

    volume

--- doc/changes.sql
+++ doc/changes.sql
@@ -1,38 +1,10 @@
-alter table add column `volume` int(2) after `kgs`;
+alter table `disposals` add column `volume` int(2) after `kgs`;
 alter table `disposals` drop column `liters`;
 
 drop table `volumes`;
 
--- no, non serve?
--- il disposal_tppe resta per tutto il processo.
-create table `deposits` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `organization_id` int(10) DEFAULT NULL,
-  `disposal_type_id` int(10) unsigned, 
-  PRIMARY KEY (`id`),
-  KEY `fk_deposits_organizations` (`organization_id`),
-  CONSTRAINT `fk_deposits_organizations` FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`id`),
-  KEY `fk_deposits_types` (`disposal_type_id`),
-  CONSTRAINT `fk_deposits_types` FOREIGN KEY (`disposal_type_id`) REFERENCES `disposal_types` (`id`)
-) CHARSET=utf8mb4;
-
--- si toglie quando si libera tutto il deposito e allora i 
--- disposals hanno completed_at
--- il problema e' che nel deposito non di differenziano quali
--- disposal si danno via.
-create table `desposits_disposals` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `deposit_id` int(10) unsigned,
-  `disposal_id` int(10) unsigned
-) CHARSET=utf8mb4;
-
--- OR
-
 -- tutti dati via, esaurito deposito
 alter table `disposals` add column `completed_at` date unsigned after `approved_at`;
-alter table `disposals` add column `deposit_id` int(10) unsigned after `liters`;
-alter table `disposals` add KEY `fk_disposals_deposits` (`deposit_id`);
-alter table `disposals` add CONSTRAINT `fk_disposals_deposits` FOREIGN KEY (`deposit_id`) REFERENCES `deposits` (`id`);
 
 create table `withdraws` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
