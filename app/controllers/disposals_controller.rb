cf8aec7b2ec97c6ffc1162f94134f4b32ca46108
Pietro <donapieppo@yahoo.it>
2021-08-30 16:25:03 +0200

    refactor

--- app/controllers/disposals_controller.rb
+++ app/controllers/disposals_controller.rb
@@ -2,7 +2,7 @@ class DisposalsController < ApplicationController
   helper DisposalHelper
 
   before_action :set_disposal_type, only: %i(new create)
-  before_action :set_disposal_and_check_permission, only: %i(show edit update destroy approve)
+  before_action :set_disposal_and_check_permission, only: %i(show edit update destroy approve unapprove)
 
   def index
     if policy(current_organization).manage?
@@ -14,6 +14,7 @@ class DisposalsController < ApplicationController
     else
       @disposals = current_user.disposals.where(organization: current_organization)
     end
+    @disposals = @disposals.order(:user_id, :created_at)
     authorize :disposal
   end
 
@@ -28,7 +29,6 @@ class DisposalsController < ApplicationController
   def new
     @disposal = current_user.disposals.new(disposal_type_id: @disposal_type.id, 
                                            organization_id: current_organization.id)
-    @volumes = {}
     authorize @disposal
   end
 
@@ -38,7 +38,6 @@ class DisposalsController < ApplicationController
     @disposal.disposal_type_id = @disposal_type.id
     authorize @disposal
     if @disposal.save
-      fix_volumes
       redirect_to disposals_path
     else
       render action: :new
@@ -46,15 +45,10 @@ class DisposalsController < ApplicationController
   end
 
   def edit
-    @volumes = {}
-    @disposal.volumes.each do |v|
-      @volumes[v.liters.to_i] = v.num.to_i
-    end
   end
 
   def update
     if @disposal.update(disposal_params)
-      fix_volumes
       redirect_to disposals_path
     else
       render action: :edit
@@ -66,23 +60,20 @@ class DisposalsController < ApplicationController
     redirect_to @disposal
   end
 
+  def unapprove
+    @disposal.unapprove!
+    redirect_to @disposal
+  end
+
+  def destroy
+    @disposal.destroy
+    redirect_to disposals_path
+  end
+
   private
 
   def disposal_params
-    params[:disposal].permit(:kgs, :liters)
-  end
-
-  # "solid"=>{"40"=>"1", "60"=>"2", "120"=>""}
-  # "liquid"=>{"10"=>"1", "20"=>"2"}
-  def fix_volumes
-    @disposal.volumes.destroy_all
-
-    h = @disposal.liquid? ? params["liquid"] : params["solid"]
-    h.each do |vol, num|
-      if num.to_i > 0
-        @disposal.volumes.create(liters: vol.to_i, num: num.to_i)
-      end
-    end
+    params[:disposal].permit(:volume, :kgs)
   end
 
   def set_disposal_type
