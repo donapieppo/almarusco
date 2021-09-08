9227189dce0a4d4c508407a6eeb794261e4a6268
Pietro <donapieppo@yahoo.it>
2021-09-08 12:38:41 +0200

    infos

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,5 +1,10 @@
 class InfosController < ApplicationController
   def index
     authorize :info
+    @infos = {}
+    current_organization.disposals.each do |disposal|
+      @infos[disposal.disposal_type] ||= []
+      @infos[disposal.disposal_type] << disposal
+    end
   end
 end
