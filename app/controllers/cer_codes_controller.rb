6ac01bd6b2d49002b3cf1566f5218c9d880b1317
Pietro <donapieppo@yahoo.it>
2021-10-11 11:49:46 +0200

    cer codes

---
+++ app/controllers/cer_codes_controller.rb
@@ -0,0 +1,48 @@
+class CerCodesController < ApplicationController
+  before_action :set_cer_code_and_check_permission, only: [:show, :edit, :update, :delete]
+
+  def index
+    authorize :cer_code
+    @cer_codes = CerCode.all
+  end
+
+  def new
+    @cer_code = CerCode.new
+    authorize @cer_code
+  end
+
+  def create
+    @cer_code = CerCode.new(cer_code_params)
+    authorize @cer_code
+    if @cer_code.save
+      redirect_to cer_codes_path, notice: "Laboratorio creato correttamente."
+    else
+      render action: :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @cer_code.update(cer_code_params)
+      redirect_to cer_codes_path, notice: "Il nome del laboratorio è stato modificato."
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_cer_code_and_check_permission
+    @cer_code = CerCode.find(params[:id])
+    authorize @cer_code
+  end
+
+  def cer_code_params
+    params[:cer_code].permit(:name, :description, :danger)
+  end
+end
--- app/models/cer_code.rb
+++ app/models/cer_code.rb
@@ -1,6 +1,7 @@
 class CerCode < ApplicationRecord
   has_many :disposal_typs
   has_many :disposals
+  has_and_belongs_to_many :suppliers
 
   def to_s
     "CER " + self.name + (self.danger ? "*" : "")
