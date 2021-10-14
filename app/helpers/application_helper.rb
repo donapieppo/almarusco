467e256183e15b44380fa97249888ac95fb7f3e0
Pietro <donapieppo@yahoo.it>
2021-10-14 09:45:33 +0200

    disposal_status_icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 include DmUniboCommonHelper
 
 module ApplicationHelper
+  def disposal_status_icon(disposal)
+    if disposal.approved?
+      '<i class="fas fa-check text-success"></i>'.html_safe
+    else
+      '<i class="fas fa-exclamation-circle text-warning"></i>'.html_safe
+    end
+  end
 end
-
