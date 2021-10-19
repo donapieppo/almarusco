838ba67fcfdcc4f9f17870db106ca4f53d247082
Pietro <donapieppo@yahoo.it>
2021-10-19 11:47:54 +0200

    icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     if disposal.approved?
       '<i class="fas fa-check text-success"></i>'.html_safe
     else
-      '<i class="fas fa-exclamation-circle text-warning"></i>'.html_safe
+      '<i class="fas fa-exclamation-circle text-danger"></i>'.html_safe
     end
   end
 end
