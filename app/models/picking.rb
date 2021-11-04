f090b0c8c47d7f564f576f34cfabdc5fbba22f11
Pietro <donapieppo@yahoo.it>
2021-11-04 11:14:23 +0100

    disposal_types_volumes_and_kgs

--- app/models/picking.rb
+++ app/models/picking.rb
@@ -11,4 +11,19 @@ class Picking < ApplicationRecord
     self.disposals = self.possible_disposals
     self.save
   end
+
+  # res[disposal_type][:volumes]["20"][1]
+  def disposal_types_volumes_and_kgs
+    res = Hash.new { |hash, key| hash[key] = {} }
+
+    self.disposals.includes(:disposal_type).each do |disposal|
+      p disposal
+      res[disposal.disposal_type]['volumes'] ||= {}
+      res[disposal.disposal_type]['volumes'][disposal.volume.to_s] = res[disposal.disposal_type]['volumes'][disposal.volume.to_s].to_i + 1
+      res[disposal.disposal_type]['kgs'] = res[disposal.disposal_type]['kgs'].to_i + disposal.kgs
+      p res
+    end
+
+    res
+  end
 end
