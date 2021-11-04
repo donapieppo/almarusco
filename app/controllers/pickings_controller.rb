bbf583cac477077ea7b3b754892e7c6edaba2656
Pietro <donapieppo@yahoo.it>
2021-11-04 14:54:51 +0100

    print csv

--- app/controllers/pickings_controller.rb
+++ app/controllers/pickings_controller.rb
@@ -1,3 +1,4 @@
+require 'csv'
 class PickingsController < ApplicationController
   before_action :set_picking_and_check_permission, only: [:show, :edit, :update, :delete, :print]
 
@@ -44,8 +45,18 @@ class PickingsController < ApplicationController
         pdf = PickingPrint.new(@picking)
         send_data pdf.render, filename: "stampa_ritiro_#{@picking.supplier.name}_#{@picking.date}.pdf", type: 'application/pdf', disposition: 'inline'
       end
+      format.csv do
+        res = CSV.generate(headers: true, col_sep: ";", quote_char: '"') do |csv|
+          csv << ["CER", "Stato fisico", "Descrizione rifiuto", "Tipo di imbal.ggio", "N° e tipo di colli", "Peso (Kg)", "Caratteristiche di pericolo", "ADR", "N. ONU", "Classe ADR"]
+          @volumes_and_kgs.each do |disposal_type, vols_and_kgs|   
+            csv << csv_extraction(disposal_type, vols_and_kgs)
+          end
+        end
+        send_data res, filename: "stampa_ritiro_#{@picking.supplier.name}_#{@picking.date}.csv"
+      end
       format.html
     end
+    @no_menu = true
   end
 
   private
@@ -57,5 +68,22 @@ class PickingsController < ApplicationController
 
   def picking_params
     params[:picking].permit(:date, disposal_ids: [])
+  end
+
+  def csv_extraction(disposal_type, vols_and_kgs)
+    res = []
+    res << disposal_type.cer_code
+    res << disposal_type.physical_state_to_s
+    res << disposal_type.cer_code.description
+    res << "Tanica"
+    res << vols_and_kgs['volumes'].keys.map(&:to_i).sort.map do |v| 
+             "da #{v} litri: #{vols_and_kgs['volumes'][v.to_s]}"
+            end.join("")
+    res << vols_and_kgs['kgs']
+    res << disposal_type.hp_codes_to_s
+    res << disposal_type.adr ? 'si' : ''
+    res << disposal_type.un_code
+    res << "???"
+    res
   end
 end
