9eb0101b74c20e78f0e7860a3bec162166ab6ee5
Pietro <donapieppo@yahoo.it>
2021-11-25 11:18:14 +0100

    adrs and pittograms

--- doc/changes.sql
+++ doc/changes.sql
@@ -1,52 +1,61 @@
-alter table `disposals` add column `delivered_at` date after `approved_at`;
-alter table `pickings` add column `delivered_at` date after `created_at`;
-
-/* create table `cer_codes_suppliers` ( */
-/*   `cer_code_id` int(10) unsigned NOT NULL, */
-/*   `supplier_id` int(10) unsigned NOT NULL, */
-/*   PRIMARY KEY (`cer_code_id, supplier_id`), */
-/*   KEY `fk_cer_codes_crsupp` (`cer_code_id`), */
-/*   KEY `fk_suppliers_crsupp` (`supplier_id`), */ 
-/*   CONSTRAINT `fk_cer_codes_crsupp` FOREIGN KEY (`cer_code_id`) REFERENCES `cer_codes` (`id`), */
-/*   CONSTRAINT `fk_suppliers_crsupp` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`) */
-/* ) CHARSET=utf8mb4;; */  
-
-create table `pickings` (
+CREATE TABLE `adrs` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `organization_id` int(10) unsigned NOT NULL,
-  `supplier_id` int(10) unsigned NOT NULL,
-  `date` date,
-  `created_at` datetime,
+  `name` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_pickings_organizations` (`organization_id`),
-  CONSTRAINT `fk_pickings_organizations` FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`id`),
-  KEY `fk_pickings_suppliers` (`supplier_id`),
-  CONSTRAINT `fk_pickings_suppliers` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`)
+  KEY `name` (`name`)
 ) CHARSET=utf8mb4;
 
-alter table `disposals` add column `picking_id` int(10) unsigned DEFAULT NULL after `lab_id`;
-alter table `disposals` add KEY `fk_disposals_pickings` (`picking_id`);
-alter table `disposals` add CONSTRAINT `fk_disposals_pickings` FOREIGN KEY (`picking_id`) REFERENCES `pickings` (`id`);
+CREATE TABLE `adrs_disposal_types` (
+  `adr_id` int(10) unsigned DEFAULT NULL,
+  `disposal_type_id` int(10) unsigned DEFAULT NULL,
+  KEY `fk_adrdt_adr` (`adr_id`),
+  KEY `fk_adrdt_dt` (`disposal_type_id`),
+  CONSTRAINT `fk_adrdt_adr` FOREIGN KEY (`adr_id`) REFERENCES `adrs` (`id`),
+  CONSTRAINT `fk_adrdt_dt` FOREIGN KEY (`disposal_type_id`) REFERENCES `disposal_types` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `pictograms` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `filename` varchar(255),
+  PRIMARY KEY (`id`)
+) CHARSET=utf8mb4;
+
+INSERT INTO `pictograms` VALUES (10, '1',   'adrs/adr_1.svg');
+INSERT INTO `pictograms` VALUES (20, '2.1', 'adrs/adr_2.1.svg');
+INSERT INTO `pictograms` VALUES (30, '2.2', 'adrs/adr_2.2.svg');
+INSERT INTO `pictograms` VALUES (40, '2.3', 'adrs/adr_2.3.svg');
+INSERT INTO `pictograms` VALUES (50, '3',   'adrs/adr_3.svg');
+INSERT INTO `pictograms` VALUES (60, '4.1', 'adrs/adr_4.1.svg');
+INSERT INTO `pictograms` VALUES (70, '4.2', 'adrs/adr_4.2.svg');
+INSERT INTO `pictograms` VALUES (80, '4.3', 'adrs/adr_4.3.svg');
+INSERT INTO `pictograms` VALUES (90, '5.1', 'adrs/adr_5.1.svg');
+INSERT INTO `pictograms` VALUES (100, '5.2', 'adrs/adr_5.2.svg');
+INSERT INTO `pictograms` VALUES (110, '6.1', 'adrs/adr_6.1.svg');
+INSERT INTO `pictograms` VALUES (120, '6.2', 'adrs/adr_6.2.svg');
+INSERT INTO `pictograms` VALUES (130, '7b', 'adrs/adr_7b.svg');
+INSERT INTO `pictograms` VALUES (140, '8',  'adrs/adr_8.svg');
+INSERT INTO `pictograms` VALUES (150, '9',  'adrs/adr_9.svg');
+INSERT INTO `pictograms` VALUES (160, '9a', 'adrs/adr_9a.svg');
+
+INSERT INTO `pictograms` VALUES (200, 'ghs01', 'ghs/ghs01.png');
+INSERT INTO `pictograms` VALUES (210, 'ghs01', 'ghs/ghs02.png');
+INSERT INTO `pictograms` VALUES (220, 'ghs01', 'ghs/ghs03.png');
+INSERT INTO `pictograms` VALUES (230, 'ghs01', 'ghs/ghs04.png');
+INSERT INTO `pictograms` VALUES (240, 'ghs01', 'ghs/ghs05.png');
+INSERT INTO `pictograms` VALUES (250, 'ghs01', 'ghs/ghs06.png');
+INSERT INTO `pictograms` VALUES (260, 'ghs01', 'ghs/ghs07.png');
+INSERT INTO `pictograms` VALUES (270, 'ghs01', 'ghs/ghs08.png');
+INSERT INTO `pictograms` VALUES (280, 'ghs01', 'ghs/ghs09.png');
 
 
-/* alter table `permissions` add column `producer_id` int(10) unsigned DEFAULT NULL; */
-/* alter table `permissions` add key `fk_permission_producer` (`producer_id`); */
-/* alter table `permissions` add constraint `fk_permission_producer` FOREIGN KEY (`producer_id`) REFERENCES `users` (`id`); */
-
-/* create table `labs` ( */
-/*   `id` int(10) unsigned NOT NULL AUTO_INCREMENT, */
-/*   `organization_id` int(10) DEFAULT NULL, */
-/*   `name` varchar(255), */
-/*   PRIMARY KEY (`id`), */
-/*   KEY `fk_labs_organizations` (`organization_id`), */
-/*   CONSTRAINT `fk_labs_organizations` FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`id`) */
-/* ) CHARSET=utf8mb4; */
-
-/* alter table `disposals` add column `producer_id`  int(10) unsigned DEFAULT NULL after `user_id`; */
-/* alter table `disposals` add KEY `fk_disposals_producers` (`producer_id`); */
-/* alter table `disposals` add CONSTRAINT `fk_disposals_producers` FOREIGN KEY (`producer_id`) REFERENCES `users` (`id`); */
-
-/* alter table `disposals` add column `lab_id`  int(10) unsigned DEFAULT NULL after `user_id`; */
-/* alter table `disposals` add KEY `fk_disposals_labs` (`lab_id`); */
-/* alter table `disposals` add CONSTRAINT `fk_disposals_labs` FOREIGN KEY (`lab_id`) REFERENCES `labs` (`id`); */
+CREATE TABLE `disposal_types_pictograms` (
+  `disposal_type_id` int(10) unsigned DEFAULT NULL,
+  `pictogram_id` int(10) unsigned DEFAULT NULL,
+  -- KEY `fk_dtp_dt` (`disposal_type_id`),
+  -- KEY `fk_dtp_picto` (`pictogram_id`),
+  CONSTRAINT `fk_dtp_dt` FOREIGN KEY (`disposal_type_id`) REFERENCES `disposal_types` (`id`),
+  CONSTRAINT `fk_dtp_picto` FOREIGN KEY (`pictogram_id`) REFERENCES `pictograms` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
