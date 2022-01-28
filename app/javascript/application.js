2b876cc9933737e54bb0cdbbb35af2494ea191b0
Pietro <donapieppo@yahoo.it>
2022-01-28 09:51:48 +0100

    rails7

---
+++ app/javascript/application.js
@@ -0,0 +1,11 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//import "@hotwired/turbo-rails"
+//import "controllers"
+import * as bootstrap from "bootstrap"
+import Rails from "@rails/ujs"
+Rails.start()
+
+//require("typeahead.js")
+//require("popper.js")
+
+
