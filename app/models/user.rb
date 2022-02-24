98192bd10a9d38a4da5f77a5601c11b546ffebde
Pietro <donapieppo@yahoo.it>
2022-02-24 12:53:28 +0100

    uniq

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   include DmUniboCommon::User
   has_many :disposals
 
+  # for who use can operate?
   def available_producers(org)
     producer_ids = org.permissions.where(authlevel: Rails.configuration.authlevels[:operate], 
-                                         user_id: self.id).map(&:producer_id)
+                                         user_id: self.id).map(&:producer_id).uniq
     User.find(producer_ids)
   end
 end
