cc2d98dacac639e5216fc8ccec70693c74f474c7
Pietro <donapieppo@yahoo.it>
2022-02-24 12:01:17 +0100

    pdf and margins

--- app/controllers/prints_controller.rb
+++ app/controllers/prints_controller.rb
@@ -27,11 +27,11 @@ class PrintsController < ApplicationController
       return
     end
 
-    pdf = Prawn::Document.new(page_size: 'A4') # 595.28 x 841.89
+    pdf = Prawn::Document.new(page_size: 'A4', margin: 20) # 595.28 x 841.89
     # :margin Sets the margin on all sides in points [0.5 inch]
     # :left_margin :right_margin
     pdf.font_size 8
-    pdf.define_grid(columns: 2, rows: 5, gutter: 15)
+    pdf.define_grid(columns: 2, rows: 5, gutter: 0)
     # pdf.grid.show_all
 
     # 96 x 51mm rettangolari
@@ -44,27 +44,32 @@ class PrintsController < ApplicationController
         pdf.grid(col.to_i - 1, row.to_i - 1).bounding_box do
 
           # pdf.stroke_bounds
-          pdf.text "n.#{disposal.id.to_s}      <font size='9'>#{disposal.organization.code} - #{disposal.lab}</font>", size: 12, inline_format: true
+          pdf.move_down 6 
+          pdf.indent(10) do
+            pdf.text " n.#{disposal.id.to_s}      <font size='9'>#{disposal.organization.code} - #{disposal.lab}</font>", size: 12, inline_format: true
+          end
 
-          y_position = pdf.cursor - 3 
+          y_position = pdf.cursor - 6 
 
           qr = RQRCode::QRCode.new(disposal_url(disposal))
           IO.binwrite("/tmp/gr_image_#{disposal.id}.png", qr.as_png(size: 240).to_s)
 
-          pdf.bounding_box([0, y_position], width: 70, height: 80) do
+          pdf.bounding_box([0, y_position], width: 100, height: 100) do
             # pdf.stroke_bounds
-            pdf.image "/tmp/gr_image_#{disposal.id}.png", width: 70, height: 70
+            pdf.image "/tmp/gr_image_#{disposal.id}.png", width: 80, height: 80
           end
 
-          pdf.bounding_box([70, y_position], width: 180, height: 80) do
+          pdf.bounding_box([70, y_position], width: 200, height: 120) do
             # pdf.stroke_bounds
-            if dt.un_code
-              pdf.text dt.un_code.to_s, style: 'bold', size: 20
+            
+            pdf.indent(10) do
+              if dt.un_code
+                pdf.text dt.un_code.to_s, style: 'bold', size: 20
+              end
+              pdf.text dt.cer_code.to_s, size: 20
+              pdf.text dt.hp_codes_to_s + " - " + dt.adrs_to_s, size: 14
+              pdf.text dt.physical_state_to_s.upcase
             end
-            pdf.text dt.cer_code.to_s, size: 20
-            pdf.text dt.physical_state_to_s.upcase
-            pdf.text dt.hp_codes_to_s + " - " + dt.adrs_to_s 
-            pdf.text dt.notes, size: 5
           end
 
           disposal.disposal_type.pictograms.each_with_index do |pict, i|
