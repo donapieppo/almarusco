5ab4178a7521976ed4009a1b0cc8b135f218187e
Pietro <donapieppo@yahoo.it>
2022-02-24 12:06:06 +0100

    available_producers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   include DmUniboCommon::User
   has_many :disposals
 
+  def available_producers(org)
+    producer_ids = org.permissions.where(authlevel: Rails.configuration.authlevels[:operate], 
+                                         user_id: self.id).map(&:producer_id)
+    User.find(producer_ids)
+  end
 end
