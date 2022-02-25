0d57e1704da388abaf13b32475de82f55a85ced9
Pietro <donapieppo@yahoo.it>
2022-02-25 11:27:11 +0100

    permitted_producers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :disposals
 
   # for who use can operate?
-  def available_producers(org)
+  def permitted_producers(org)
     producer_ids = org.permissions.where(authlevel: Rails.configuration.authlevels[:operate], 
                                          user_id: self.id).map(&:producer_id).uniq
     User.find(producer_ids)
