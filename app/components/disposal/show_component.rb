71da50acd162edb64b838d098d4726f5ce9f9d6b
Pietro <donapieppo@yahoo.it>
2022-03-07 14:03:00 +0100

    details

--- app/components/disposal/show_component.rb
+++ app/components/disposal/show_component.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class Disposal::ShowComponent < ViewComponent::Base
-  def initialize(current_user, disposal)
+  def initialize(current_user, disposal, with_details: true)
     @current_user = current_user
     @disposal = disposal
+    @with_details = with_details
+
     @status_icon = disposal.approved? ? '<i class="fas fa-check text-success"></i>' : '<i class="fas fa-exclamation-circle text-danger"></i>'
     @policy = DisposalPolicy.new(current_user, disposal)
   end
