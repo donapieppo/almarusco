af8c9947a6d63e40face0376bafbbebebe37c823
Pietro <donapieppo@yahoo.it>
2022-03-07 15:43:00 +0100

    in component

--- app/views/disposals/_js_infos.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".details").forEach(x => { x.style.display = "none" });
-document.querySelectorAll(".show-details").forEach(x => {
-  x.addEventListener('click', (e) => {
-    console.log("show infos");
-    e.target.style.display = "none";
-    e.target.parentElement.parentElement.parentElement.querySelector(".details").style.display = "block";
-  })
-})
-
