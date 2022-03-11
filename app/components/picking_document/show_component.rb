28f5ca8b95c2cc2d4801afa9fcd378b39078a490
Pietro <donapieppo@yahoo.it>
2022-03-11 11:42:09 +0100

    @all_ok

--- app/components/picking_document/show_component.rb
+++ app/components/picking_document/show_component.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
 class PickingDocument::ShowComponent < ViewComponent::Base
-  def initialize(current_user, picking, disposal_type, expected_kgs: nil, expected_liters: nil)
+  def initialize(current_user, picking, disposal_type, expected_kgs: nil, expected_volume: nil)
     @current_user = current_user
     @picking = picking
     @disposal_type = disposal_type
+    @expected_kgs = expected_kgs
+    @expected_volume = expected_volume
 
     @picking_document = @picking.picking_documents.where(disposal_type: @disposal_type).first   
+
+    if @picking_document && @picking_document.kgs == @expected_kgs && @picking_document.volume == @expected_volume
+      @all_ok = true
+    else
+      @all_ok = false
+    end
   end
 end
 
