55b207a3b0fdeaf38fdfb3965c170f77cb6ff4b5
Pietro <donapieppo@yahoo.it>
2022-04-07 12:44:59 +0200

    validate

--- app/models/cer_code.rb
+++ app/models/cer_code.rb
@@ -3,6 +3,8 @@ class CerCode < ApplicationRecord
   has_many :disposals
   has_and_belongs_to_many :suppliers
 
+  validates :name, presence: true
+
   def to_s
     "CER " + self.name + (self.danger ? "*" : "")
   end
