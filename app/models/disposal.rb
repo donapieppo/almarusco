fb53f15bd0c69a6307e79f95b190d1faaaf1f87b
Pietro <donapieppo@yahoo.it>
2022-04-13 14:59:36 +0200

    producer_upn

--- app/models/disposal.rb
+++ app/models/disposal.rb
@@ -13,6 +13,7 @@ class Disposal < ApplicationRecord
   scope :approved, -> { where.not(approved_at: nil) }
   scope :delivered, -> { where.not(delivered_at: nil) }
   scope :undelivered, -> { where(delivered_at: nil) }
+  scope :include_all, -> { includes(:user, :producer, :lab, disposal_type: [:cer_code, :un_code, :hp_codes, :adrs]) }
 
   def to_s
     "#{self.volume} L - #{self.kgs} Kg #{self.disposal_type.to_s_short}"
@@ -52,5 +53,18 @@ class Disposal < ApplicationRecord
 
   def undelivered?
     ! delivered?
+  end
+
+  def producer_upn
+    self.producer ? self.producer.upn : ""
+  end
+
+  # possiamo passare 'pietro.donatini' 'pietro.donatini@unibo.it' 'Pietro donatini pietro.donatini@unibo.it'
+  def producer_upn=(upn)
+    if upn =~ /(\w+\.\w+)/ 
+      @_user_upn = "#{$1}@unibo.it"
+    else
+      @_user_upn = upn
+    end
   end
 end
