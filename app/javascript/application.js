3e5e011e01b947193ef3ba08bc660c0d5c7ba961
Pietro <donapieppo@yahoo.it>
2022-05-23 15:27:21 +0200

    fontawesome

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//import "controllers"
 import * as bootstrap from "bootstrap"
+import "@fortawesome/fontawesome-free/js/all";
 import "@hotwired/turbo-rails"
 import "./controllers"
