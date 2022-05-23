e94752ddf99967f1994b222e8342580e36884ab5
Pietro <donapieppo@yahoo.it>
2022-05-23 12:30:56 +0200

    turbo modal in dm lib

--- app/components/turbo_modal/modal_component.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-include Turbo::FramesHelper
-
-class TurboModal::ModalComponent < ViewComponent::Base
-  def initialize(title: "", hidden: false)
-    @title = title
-    @hidden = hidden
-  end
-end
