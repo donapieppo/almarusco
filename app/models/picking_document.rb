e259d1f1ab506f6d129fd6eaf503c8469a0ffdc6
Pietro <donapieppo@yahoo.it>
2022-06-13 10:45:03 +0200

    rifiuti non pericolosi non hanno obbligo register_number

--- app/models/picking_document.rb
+++ app/models/picking_document.rb
@@ -5,7 +5,7 @@ class PickingDocument < ApplicationRecord
   validates :disposal_type_id, uniqueness: { scope: :picking_id, message: 'Documento già inserito per la tipologia di rifuti.' }
   validates :serial_number, uniqueness: {}
   validate :organization_coherence
-  validate :unique_in_organizations_and_year
+  validate :unique_register_number_in_organizations_and_year
 
   def to_s
     "#{self.serial_number} - #{self.register_number}"
@@ -15,12 +15,15 @@ class PickingDocument < ApplicationRecord
     (self.disposal_type.organization_id == self.picking.organization_id) or self.errors.add(:base, "UL incoerenti")
   end
 
-  def unique_in_organizations_and_year
+  def unique_register_number_in_organizations_and_year
+    # not adr (not danger, has no register_number)
+    return true unless disposal_type.adr
+
     organization = self.picking.organization
     year = self.picking.date.year
     pids = organization.pickings.where('YEAR(date) = ?', year).ids
     if PickingDocument.where.not(id: self.id).where(register_number: self.register_number).where(picking_id: pids).any?
-      self.errors.add(:register_number, "Non univoco nell'anno #{year}")
+      self.errors.add(:register_number, "non univoco nell'anno #{year}")
     end
   end
 
