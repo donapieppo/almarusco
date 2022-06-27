7980f6b142ced4d66bedc1f63ce538d23a1dabf8
Pietro <donapieppo@yahoo.it>
2022-06-27 12:09:03 +0200

    deposit in dates

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -1,10 +1,16 @@
 class DepositsController < ApplicationController
   def index
     authorize :deposit
+    @at = params[:at]
     @deposit = {}
+    @approved_at = []
     current_organization.disposals.approved.undelivered.includes(disposal_type: [:cer_code, :un_code, :hp_codes]).each do |disposal|
-      @deposit[disposal.disposal_type] ||= []
-      @deposit[disposal.disposal_type] << disposal
+      if (! @at) || @at == disposal.approved_at.to_s
+        @deposit[disposal.disposal_type] ||= []
+        @deposit[disposal.disposal_type] << disposal
+      end
+      @approved_at << disposal.approved_at
     end
+    @approved_at = @approved_at.sort.uniq
   end
 end
