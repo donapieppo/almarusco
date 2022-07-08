630fecc3f60e82f90cb70012983ab952c0b0a51a
Pietro <donapieppo@yahoo.it>
2022-07-08 09:22:27 +0200

    set_locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < DmUniboCommon::ApplicationController
   include DmUniboCommon::Controllers::Helpers
 
-  before_action :set_current_user, :update_authorization, :set_current_organization, :log_current_user, :force_sso_user
+  before_action :set_current_user, :update_authorization, :set_current_organization, :log_current_user, :force_sso_user, :set_locale
   after_action :verify_authorized, except: [:who_impersonate, :impersonate, :stop_impersonating]
 
   def set_locale
