66be1fa9dd85c8e44de7d97348dca811a825c26c
Pietro <donapieppo@yahoo.it>
2022-07-08 11:51:51 +0200

    cer-selector -> disposal-selector

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,7 @@
 import { application } from "./application"
 
-import CerSelectorController from "./cer_selector_controller"
-application.register("cer-selector", CerSelectorController)
+import DisposalSelectorController from "./disposal_selector_controller"
+application.register("disposal-selector", DisposalSelectorController)
 import DisposalTypeController from "./disposal_type"
 application.register("disposal-type", DisposalTypeController)
 import DsaAwesomplete from "./dsa_awesomplete.js"
