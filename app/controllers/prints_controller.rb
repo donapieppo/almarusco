81bba341c306f987a6e559c186953a3fab24a793
Pietro <donapieppo@yahoo.it>
2022-07-20 11:17:15 +0200

    print

--- app/controllers/prints_controller.rb
+++ app/controllers/prints_controller.rb
@@ -33,11 +33,12 @@ class PrintsController < ApplicationController
     end
 
     # pdf = Prawn::Document.new(page_size: 'A4', margin: 20) # 595.28 x 841.89
-    pdf = Prawn::Document.new(page_size: 'A4', margin_top: 6.mm, margin_bottom: 4.mm, margin_left: 6.mm, margin_right: 8.mm) # 595.28 x 841.89
+    # pdf = Prawn::Document.new(page_size: 'A4', margin_top: 1.mm, margin_bottom: 1.mm, margin_left: 6.mm, margin_right: 8.mm) # 595.28 x 841.89
+    pdf = Prawn::Document.new(page_size: 'A4', margin: 6.mm) # 595.28 x 841.89
     # :margin Sets the margin on all sides in points [0.5 inch] :left_margin :right_margin
     pdf.font_size 8
     pdf.define_grid(columns: 2, rows: 5, gutter: 0)
-    # pdf.grid.show_all
+    pdf.grid.show_all
 
     # 96 x 51mm rettangolari
     # ["1-1", "2-2", "1-5"]
@@ -45,43 +46,43 @@ class PrintsController < ApplicationController
       row, col = rc.split('-')
       if disposal = @disposals.pop
         dt = disposal.disposal_type
+        qr = RQRCode::QRCode.new(disposal_url(disposal))
+        IO.binwrite("/tmp/gr_image_#{disposal.id}.png", qr.as_png(size: 240).to_s)
+
         # :rows, :columns, :gutter, :row_gutter, :column_gutter 
         pdf.grid(col.to_i - 1, row.to_i - 1).bounding_box do
 
           # pdf.stroke_bounds
-          pdf.move_down 8 
+          pdf.move_down 8
           pdf.indent(10) do
-            pdf.text " n.#{disposal.id.to_s}      <font size='9'>#{disposal.organization.code} - #{disposal.lab}</font>", size: 12, inline_format: true
+            pdf.text " n.#{disposal.id.to_s}    <font size='9'>#{disposal.organization.code} - #{disposal.lab}</font>", size: 14, inline_format: true
           end
 
           y_position = pdf.cursor - 6 
 
-          qr = RQRCode::QRCode.new(disposal_url(disposal))
-          IO.binwrite("/tmp/gr_image_#{disposal.id}.png", qr.as_png(size: 240).to_s)
-
-          pdf.bounding_box([0, y_position], width: 100, height: 100) do
+          pdf.bounding_box([10, y_position], width: 80, height: 80) do
             # pdf.stroke_bounds
             pdf.image "/tmp/gr_image_#{disposal.id}.png", width: 80, height: 80
           end
 
-          pdf.bounding_box([70, y_position], width: 180, height: 120) do
+          pdf.bounding_box([90, y_position], width: 180, height: 120) do
             # pdf.stroke_bounds
             
-            pdf.indent(10) do
+            pdf.indent(5) do
               if dt.un_code
                 pdf.text dt.un_code.to_s, style: 'bold', size: 16
               end
               pdf.text dt.cer_code.to_s, size: 16
               pdf.text dt.hp_codes_to_s + " - " + dt.adrs_to_s, size: 8
-              pdf.text dt.physical_state_to_s.upcase, size: 6
+              pdf.text dt.physical_state_to_s.upcase, size: 8
             end
           end
 
           disposal.disposal_type.pictograms.each_with_index do |pict, i|
             if pict.filename =~ /\.png/
-              pdf.image open(pict.full_filename), height: 24, at: [80+26*i, 40]
+              pdf.image open(pict.full_filename), height: 24, at: [10+26*i, 40]
             else
-              pdf.svg IO.read(pict.full_filename), height: 24, at: [80+26*i, 40]
+              pdf.svg IO.read(pict.full_filename), height: 24, at: [10+26*i, 40]
             end
           end
 
