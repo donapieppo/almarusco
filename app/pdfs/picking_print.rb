d9f072e252ae59e02fdc834880852ff26fe9397c
Pietro <donapieppo@yahoo.it>
2022-07-29 14:44:48 +0200

    font and color

--- app/pdfs/picking_print.rb
+++ app/pdfs/picking_print.rb
@@ -4,19 +4,20 @@ class PickingPrint
   def initialize(picking, data)
     @picking = picking
     @document = Prawn::Document.new(page_size: 'A4', page_layout: :landscape, font_size: 8)
+    font_size 8
     header
     content(data)
   end
 
   def header
-    text "#{@picking.supplier.name.upcase} MODULO PRENOTAZIONE SERVIZIO", style: :bold
+    text "#{@picking.supplier.name.upcase} MODULO PRENOTAZIONE SERVIZIO", style: :bold, size: 10
     move_down 10
     table [["Unità locale: ", "#{@picking.organization.to_s.upcase} #{@picking.organization.description.to_s.upcase}"],
            ["Ritiro per il giorno: ", "#{I18n.l @picking.date}"],
-           ["Contatti responsabili ul: ", "#{@picking.contact.to_s.upcase}"],
-           ["Luogo del ritiro: ", "#{@picking.address.to_s.upcase}"]] do
+           ["Contatti responsabili ul: ", "#{@picking.contact}"],
+           ["Luogo del ritiro: ", "#{@picking.address}"]] do
       cells.borders = [] 
-      cells.background_color = 'eeeeee'
+      cells.background_color = 'FFCA2C'
       column(0).align = :right
     end
     move_down 20
@@ -39,7 +40,6 @@ class PickingPrint
   end
 
   def content(data)
-    font_size 8
     # @volumes_and_kgs.each do |disposal_type, vols_and_kgs|   
     #   lines << extraction(disposal_type, vols_and_kgs)
     # end
