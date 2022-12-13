b7c203b82208ecf392f3ff1c6148ee47168fbb8f
Pietro <donapieppo@yahoo.it>
2022-12-13 11:39:19 +0100

    pictograms

--- app/helpers/disposal_helper.rb
+++ app/helpers/disposal_helper.rb
@@ -4,12 +4,6 @@ module DisposalHelper
     image_tag qr.as_png(size: 180).to_data_url
   end
 
-  def disposal_type_pictograms(disposal_type, size: 100)
-    disposal_type.pictograms.each do |pictogram|
-      concat image_tag(pictogram.image, size: size) 
-    end
-  end
-
   def adr_images(disposal)
     disposal.disposal_type.adr_classes.each do |adrc|
       concat image_tag("labels/adr_#{adrc}.svg", size: 100)
