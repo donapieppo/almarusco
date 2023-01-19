46ab94eb85dd1b770b85992c2c4decf646949097
Pietro <donapieppo@yahoo.it>
2023-01-19 15:12:01 +0100

    clean

--- app/controllers/disposals_controller.rb
+++ app/controllers/disposals_controller.rb
@@ -41,6 +41,7 @@ class DisposalsController < ApplicationController
 
   def show
     @modal = params[:modal]
+    render layout: false if modal_page
   end
 
   def choose_disposal_type
@@ -101,7 +102,6 @@ class DisposalsController < ApplicationController
       res = @disposal.approve!
       format.html { redirect_to @disposal }
       format.turbo_stream
-      format.js
     end
   end
 
@@ -110,7 +110,6 @@ class DisposalsController < ApplicationController
       res = @disposal.unapprove!
       format.html { redirect_to @disposal }
       format.turbo_stream
-      format.js
     end
   end
 
@@ -144,21 +143,21 @@ class DisposalsController < ApplicationController
     @disposal_types = current_organization.disposal_types.where(id: @disposals.map(&:disposal_type_id).sort.uniq)
   end
 
-  # keep here (in conroller and not in model legalize_all because you legalize what you see (from deposit#logalize) 
-  # "disposal_ids"=>"898,1311,1349,1422,1519"
-  def legalize
-    authorize :disposal
-    if params[:register_number].to_i == 0
-      flash[:alert] = "Manca il numero della registrazione."
-    elsif RegisterNumber.new(params[:register_number], current_organization).invalid?
-      flash[:alert] = "Numero della registrazione già utilizato precedentemente."
-    else
-      current_organization.disposals.approved.unlegalized.find(params[:disposal_ids].split(',')).each do |disposal|
-        disposal.legalize!(params[:register_number].to_i)
-      end
-    end
-    redirect_to to_legalize_path # depositis#to_legalize
-  end
+  # # keep here (in conroller and not in model legalize_all because you legalize what you see (from deposit#logalize) 
+  # # "disposal_ids"=>"898,1311,1349,1422,1519"
+  # def legalize
+  #   authorize :disposal
+  #   if params[:register_number].to_i == 0
+  #     flash[:alert] = "Manca il numero della registrazione."
+  #   elsif RegisterNumber.new(params[:register_number], current_organization).invalid?
+  #     flash[:alert] = "Numero della registrazione già utilizato precedentemente."
+  #   else
+  #     current_organization.disposals.approved.unlegalized.find(params[:disposal_ids].split(',')).each do |disposal|
+  #       disposal.legalize!(params[:register_number].to_i)
+  #     end
+  #   end
+  #   redirect_to to_legalize_path # depositis#to_legalize
+  # end
 
   private
 
