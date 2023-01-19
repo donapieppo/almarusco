2713758249500e686d6487a10469644cc6ec1f35
Pietro <donapieppo@yahoo.it>
2023-01-19 14:59:45 +0100

    new component

---
+++ app/components/disposal/group_details_component.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Disposal::GroupDetailsComponent < ViewComponent::Base
+  def initialize(disposals)
+    @volume_data = Hash.new {|h, k| h[k] = { units: 0, kgs: 0.0 }} 
+
+    disposals.each do |disposal|
+      @volume_data[disposal.volume][:units] += disposal.units 
+      @volume_data[disposal.volume][:kgs] += disposal.kgs
+    end
+  end
+end
+
