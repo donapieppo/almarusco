9158ee6397dbbf15c299472674dc24546933e453
Pietro <donapieppo@yahoo.it>
2023-01-23 13:41:26 +0100

    update

--- app/controllers/legal_downloads_controller.rb
+++ app/controllers/legal_downloads_controller.rb
@@ -1,6 +1,6 @@
 class LegalDownloadsController < LegalRecordsController
   before_action :set_picking_document_and_disposal_type, only: [:new, :create]
-  before_action :set_legal_download_and_check_permission, only: [:show, :edit, :update, :delete]
+  before_action :set_legal_download_and_disposal_type_and_check_permission, only: [:show, :edit, :update, :delete]
 
   def new
     @legal_download = current_organization.legal_downloads.new(picking_document: @picking_document, disposal_type: @disposal_type, date: Date.today)
@@ -11,7 +11,6 @@ class LegalDownloadsController < LegalRecordsController
     @legal_download = current_organization.legal_downloads.new(picking_document: @picking_document,
                                                                disposal_type: @disposal_type, 
                                                                number: params[:legal_download][:number], 
-                                                               year: Date.today.year, 
                                                                date: params[:legal_download][:date])
     authorize @legal_download
     if @legal_download.save
@@ -27,7 +26,15 @@ class LegalDownloadsController < LegalRecordsController
   end
 
   def edit
-    @disposal_type = @legal_download.disposal_type
+  end
+
+  def update
+    if @legal_download.update(number: params[:legal_download][:number],
+                              date: params[:legal_download][:date])
+      redirect_to @legal_download, notice: "Registrazione scarico effettuata con numero #{@legal_download.number}"
+    else
+      render action: :edit, status: :unprocessable_entity
+    end
   end
 
   private
@@ -37,8 +44,9 @@ class LegalDownloadsController < LegalRecordsController
     @disposal_type = @picking_document.disposal_type
   end
 
-  def set_legal_download_and_check_permission
+  def set_legal_download_and_disposal_type_and_check_permission
     @legal_download = current_organization.legal_downloads.find(params[:id])
+    @disposal_type = @legal_download.disposal_type
     authorize @legal_download
   end
 end
