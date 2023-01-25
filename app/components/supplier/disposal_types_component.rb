77bc1966ff5485cbf03ec7c9508459507cab260b
Pietro <donapieppo@yahoo.it>
2023-01-25 11:01:04 +0100

    @disposal_types

--- app/components/supplier/disposal_types_component.rb
+++ app/components/supplier/disposal_types_component.rb
@@ -2,5 +2,6 @@ class Supplier::DisposalTypesComponent < ViewComponent::Base
   def initialize(supplier, organization_id)
     @supplier = supplier
     @organization_id = organization_id
+    @disposal_types = @supplier.contract_disposal_types(@organization_id).includes(:cer_code, :un_code, :hp_codes).order(:cer_code_id).to_a
   end
 end
