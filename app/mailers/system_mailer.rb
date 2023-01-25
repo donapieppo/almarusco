6e70cec33b312163dfc8d2bb7e2bc0c314775199
Pietro <donapieppo@yahoo.it>
2023-01-25 11:01:38 +0100

    mail

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -1,4 +1,15 @@
 class SystemMailer < ApplicationMailer
+  def notify_expiration(permission)
+    @user = permission.user
+    @organization = permission.organization
+    @expiration_date = permission.expiry
+    @producer = User.find(permission.producer_id)
+
+    mail(to:      @user.upn, 
+         bcc:     'pietro.donatini@unibo.it', 
+         subject: "Avviso scadenza accesso applicativo AlmaRusco.")
+  end
+
   def notify_approved_disposals(user, disposals)
     @user      = user
     @disposals = disposals
