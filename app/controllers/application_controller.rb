00f604a38f874d938b2d466f4c33c1b32999599a
Pietro <donapieppo@yahoo.it>
2023-01-30 16:29:55 +0100

    check_current_organization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < DmUniboCommon::ApplicationController
   include DmUniboCommon::Controllers::Helpers
 
-  before_action :set_current_user, :update_authorization, :set_current_organization, :log_current_user, :force_sso_user, :set_locale
+  before_action :set_current_user, :update_authorization, :set_current_organization, :log_current_user, :force_sso_user, :check_current_organization, :set_locale
   after_action :verify_authorized, except: [:who_impersonate, :impersonate, :stop_impersonating]
+
+  def check_current_organization
+    if ! current_organization
+      redirect_to no_access_path
+    end
+  end
 
   def set_locale
     I18n.locale = :it
