02a27142d33bc330adf45fef03f53ac06f205726
Pietro <donapieppo@yahoo.it>
2023-01-31 08:38:24 +0100

    check auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < DmUniboCommon::ApplicationController
   include DmUniboCommon::Controllers::Helpers
 
-  before_action :set_current_user, :update_authorization, :set_current_organization, :log_current_user, :force_sso_user, :check_current_organization, :set_locale
+  before_action :set_current_user, :update_authorization, :set_current_organization, :log_current_user, :force_sso_user, :after_current_user_and_organization, :set_locale
   after_action :verify_authorized, except: [:who_impersonate, :impersonate, :stop_impersonating]
 
-  def check_current_organization
-    if ! current_organization
-      redirect_to no_access_path
+  def after_current_user_and_organization
+    if ! current_user || ! current_organization || ! OrganizationPolicy.new(current_user, current_organization).dispose?
+      skip_authorization
+      redirect_to dm_unibo_common.no_access_path
+      return
     end
   end
 
