760adfaf34e5aa25ac06f19b03fb9b6f15c893cb
Pietro <donapieppo@yahoo.it>
2023-01-31 15:16:42 +0100

    typo and name uniqueness

--- app/models/cer_code.rb
+++ app/models/cer_code.rb
@@ -1,9 +1,9 @@
 class CerCode < ApplicationRecord
-  has_many :disposal_typs
+  has_many :disposal_types
   has_many :disposals
   has_and_belongs_to_many :suppliers
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   def to_s
     "CER " + self.name + (self.danger ? "*" : "")
