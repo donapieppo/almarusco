12e2f70a1f8b5eadbaa69eb6e2b1d80b974a4267
Pietro <donapieppo@yahoo.it>
2023-02-01 09:38:24 +0100

    refactor

--- app/components/disposal/status_component.rb
+++ app/components/disposal/status_component.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class Disposal::StatusComponent < ViewComponent::Base
-  def initialize(current_user, disposal, modal: false)
+  def initialize(current_user, disposal, no_link: false)
     @current_user = current_user
     @disposal = disposal
-    @modal = modal
-
-    @policy = DisposalPolicy.new(@current_user, @disposal)
+    @no_link = no_link
 
     @status = @disposal.status
     @status_icon = disposal.approved? ? '<i class="fas fa-check text-success"></i>' : '<i class="fas fa-exclamation-circle text-danger"></i>'
+
+    @policy = DisposalPolicy.new(@current_user, @disposal)
   end
 end
