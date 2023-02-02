bf3709bf93d18d6e13bb2668651ad46c30ee6724
Pietro <donapieppo@yahoo.it>
2023-02-02 10:43:12 +0100

    factories with default kgs > 0

--- test/factories/disposal.rb
+++ test/factories/disposal.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     user
     disposal_type { association :disposal_type, organization: organization }
     lab { association :lab, organization: organization }
+    kgs { 2 }
     volume { 10 }
   end
 end
