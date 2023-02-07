963a792e3cbb0b2048e7a21256168445644944e1
Pietro <donapieppo@yahoo.it>
2023-02-07 12:25:32 +0100

    order

--- app/controllers/legal_records_controller.rb
+++ app/controllers/legal_records_controller.rb
@@ -1,7 +1,7 @@
 class LegalRecordsController < ApplicationController
   def index
     authorize :legal_record
-    @legal_records = current_organization.legal_records.order(:number)
+    @legal_records = current_organization.legal_records.order('year desc, number desc')
   end
 
   def todo
