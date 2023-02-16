b8add532ce3ac763161b1668222ef17eccf35221
Pietro <donapieppo@yahoo.it>
2023-02-16 16:01:03 +0100

    buildings

---
+++ app/javascript/controllers/disposal_lab_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "building", "labs" ]
+
+  connect() {
+    console.log("disposal lab selector connected");
+  }
+
+  change_building(e) {
+    console.log(`change building -> ${e.target.value}`);
+    this.labsTarget.querySelectorAll("option").forEach( (o) => {
+      o.style.display = (o.dataset.buildingId == e.target.value) ? 'block' : 'none';
+    });
+  }
+}
