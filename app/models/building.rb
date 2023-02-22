1b99728ab6b2d5bdbdb195f6f96164a76c89be46
Pietro <donapieppo@yahoo.it>
2023-02-22 16:25:47 +0100

    validates

--- app/models/building.rb
+++ app/models/building.rb
@@ -2,7 +2,7 @@ class Building < ApplicationRecord
   belongs_to :organization
   has_many :labs
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { scope: :organization_id, message: 'Edificio con lo stesso nome già presente.' }
 
   def to_s
     self.name
