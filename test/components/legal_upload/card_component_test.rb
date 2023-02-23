d9951d7b37ab38404e43032738540eac467a6df7
Pietro <donapieppo@yahoo.it>
2023-02-23 15:17:03 +0100

    test

---
+++ test/components/legal_upload/card_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class LegalUpload::CardComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(LegalUpload::CardComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
