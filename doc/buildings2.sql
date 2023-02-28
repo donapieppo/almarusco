88eaf9083b63a3e4572c53944731b6bf45578552
Pietro <donapieppo@yahoo.it>
2023-02-28 16:47:11 +0100

    changes

---
+++ doc/buildings2.sql
@@ -0,0 +1,8 @@
+update buildings set name="U.e.4", address="via Gobetti 85", description="ricerca di CHIM e CHIMIND"  where organization_id=47;
+update buildings set name="ED 225", address="Via Selmi 2", description="" where organization_id=23;
+
+insert into buildings values(0, 47, "U.e.4", "Via Gobetti 85", "Ricerca di CHIM e CHIMIND");
+insert into buildings values(0, 47, "U.e.5", "Via Gobetti 87", "Didattica + ricerca computazionale Fabit");
+insert into buildings values(0, 47, "U.e.6", "Via Bignardi 15", "Ricerca Fabit");
+
+insert into buildings values(0, 23, "ED 441", "Via S. Giacomo 7", "");
