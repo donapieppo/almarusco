d26a4bdd0ac1146b2f8eb46c4d3f3a5d513e051d
Pietro <donapieppo@yahoo.it>
2023-03-03 12:30:50 +0100

    reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,14 @@
 class ReportsController < ApplicationController
   def index
     authorize :report
-    @supplier = Supplier.find(4)
+    if params[:supplier_id]
+      @supplier = Supplier.find(params[:supplier_id])
+      @pickings = @supplier.pickings.where("date > DATE_SUB(NOW(), INTERVAL 3 MONTH)")
+                                    .where('completed_at is not null')
+                                    .includes(:organization, picking_documents: [disposal_type: :cer_code])
+                                    .order(:organization_id)
+    else
+      @pickings = []
+    end
   end
 end
