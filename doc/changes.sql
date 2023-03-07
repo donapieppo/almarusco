ee60b867810c4c93ae783221fe10cf3a00b873cf
Pietro <donapieppo@yahoo.it>
2023-03-07 10:06:54 +0100

    fix legal_record_id

--- doc/changes.sql
+++ doc/changes.sql
@@ -3,3 +3,15 @@
 -- alter table disposals add KEY `k_local_id` (`local_id`);
 -- update permissions set expiry ='2023-01-31' where expiry <= '2023-01-01';
 
+-- select * from disposals where legalized_at is null and delivered_at is not null;
+-- select * from disposals where legalized_at is null and delivered_at is not null;
+-- select * from disposals where legalized_at > delivered_at and legal_record_id is not null;
+-- select * from disposals where legalized_at is not null and legal_record_id is null order by legalized_at;
+
+update disposals set legalized_at = delivered_at where legalized_at > delivered_at and legal_record_id is null;
+update disposals set legalized_at = delivered_at where delivered_at is not null and legalized_at is null;
+
+update disposals set legal_record_id = NULL where legalized_at > delivered_at and legal_record_id is not null;
+
+update disposals set legalized_at = delivered_at where legalized_at > delivered_at and legal_record_id is null;
+
