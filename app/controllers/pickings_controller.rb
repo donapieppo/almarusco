ea943b6050faddc006c09afbb33475123ae607ff
Pietro <donapieppo@yahoo.it>
2023-03-13 12:12:58 +0100

    syntax

--- app/controllers/pickings_controller.rb
+++ app/controllers/pickings_controller.rb
@@ -4,7 +4,7 @@ class PickingsController < ApplicationController
 
   def index
     authorize :picking
-    @pickings = current_organization.pickings.includes(:supplier, :picking_documents).order('date desc')
+    @pickings = current_organization.pickings.includes(:supplier, :picking_documents).order("date desc")
   end
 
   def show
@@ -17,7 +17,7 @@ class PickingsController < ApplicationController
 
   # more a choose_supplier than a new :-)
   def new
-    @current_pickings = current_organization.pickings.undelivered.all # da fissare con all prima di aggiungere new 
+    @current_pickings = current_organization.pickings.undelivered.all # da fissare con all prima di aggiungere new
     @suppliers = Supplier.where.not(id: @current_pickings.pluck(:supplier_id))
     @picking = current_organization.pickings.new
     authorize @picking
@@ -29,17 +29,17 @@ class PickingsController < ApplicationController
     authorize @picking
     if @picking.save
       @picking.fill_with_default_disposals
-      redirect_to edit_picking_path(@picking), notice: 'Ritiro creato.'
+      redirect_to edit_picking_path(@picking), notice: "Il ritiro è stato correttamente creato."
     else
-      redirect_to root_path, alert: 'ERRORE'
+      redirect_to root_path, alert: "ERRORE"
     end
   end
 
   def edit
-    @can_change_disposals = ! @picking.delivered?
+    @can_change_disposals = !@picking.delivered?
     if @can_change_disposals
       @possible_disposals = @picking.possible_disposals.order(:id).includes(:user, :producer, disposal_type: [:un_code, :cer_code])
-    end 
+    end
     @previous_contacts = current_organization.pickings.where("contact > ''").select(:contact).group(:contact).distinct.map(&:contact).sort
     @previous_address = current_organization.pickings.where("address > ''").select(:address).group(:address).distinct.map(&:address).sort
   end
@@ -60,7 +60,7 @@ class PickingsController < ApplicationController
   def print_request
     filename = "stampa_ritiro_#{@picking.supplier.name}_#{@picking.date}"
     pdf = PickingPrint.new(@picking, params.to_unsafe_hash)
-    send_data pdf.render, filename: "#{filename}.pdf", type: 'application/pdf', disposition: 'inline'
+    send_data pdf.render, filename: "#{filename}.pdf", type: "application/pdf", disposition: "inline"
   end
 
   def deliver
@@ -79,7 +79,11 @@ class PickingsController < ApplicationController
   private
 
   def set_picking_and_check_permission
-    @picking = current_organization.pickings.find(params[:id])
+    @picking = if current_user.nuter?
+      Picking.find(params[:id])
+    else
+      current_organization.pickings.find(params[:id])
+    end
     authorize @picking
   end
 
@@ -92,17 +96,17 @@ class PickingsController < ApplicationController
     if array_headers
       res << ["CER", "Stato fisico", "Descrizione rifiuto", "Tipo di imbal.ggio", "N° e tipo di colli", "Peso (Kg)", "Caratteristiche di pericolo", "ADR", "N. ONU", "Classe ADR"]
     end
-    volumes_and_kgs.each do |disposal_type, vols_and_kgs|   
-      volumes = vols_and_kgs[:volumes].keys.map(&:to_i).sort 
+    volumes_and_kgs.each do |disposal_type, vols_and_kgs|
+      volumes = vols_and_kgs[:volumes].keys.map(&:to_i).sort
       line = []
       line << disposal_type.cer_code.to_s
       line << disposal_type.physical_state_to_s
       line << disposal_type.cer_code.description
-      line << volumes.map {|v| v == 200 ? 'Fusto' : 'Tanica'}.sort.uniq.join(' e ')
-      line << volumes.map {|v| "da #{v} litri: #{vols_and_kgs[:volumes][v.to_s]}"}.join('')
+      line << volumes.map { |v| (v == 200) ? "Fusto" : "Tanica" }.sort.uniq.join(" e ")
+      line << volumes.map { |v| "da #{v} litri: #{vols_and_kgs[:volumes][v.to_s]}" }.join("")
       line << vols_and_kgs[:kgs]
       line << disposal_type.hp_codes_to_s
-      line << (disposal_type.adr ? 'si' : '')
+      line << (disposal_type.adr ? "si" : "")
       line << disposal_type.un_code.to_s
       line << disposal_type.adrs_to_s
       res << line
