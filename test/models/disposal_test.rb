14e726490f24828e62b61a3789cfbfba78c1109e
Pietro <donapieppo@yahoo.it>
2023-03-20 10:30:06 +0100

    danger

--- test/models/disposal_test.rb
+++ test/models/disposal_test.rb
@@ -1,8 +1,39 @@
 require "test_helper"
 
 class DisposalTest < ActiveSupport::TestCase
-  test "ciao" do
-    d = FactoryBot.create(:disposal)
-    assert_instance_of(Disposal, d)
+  test ".danger? responds true for danger cer" do
+    cer_code = FactoryBot.create(:cer_code, danger: true)
+    dt = FactoryBot.create(:disposal_type, cer_code: cer_code)
+    d = FactoryBot.create(:disposal, organization: dt.organization, disposal_type: dt)
+
+    assert d.danger?
+  end
+
+  test ".danger? responds false for not-danger cer" do
+    cer_code = FactoryBot.create(:cer_code, danger: false)
+    dt = FactoryBot.create(:disposal_type, cer_code: cer_code)
+    d = FactoryBot.create(:disposal, organization: dt.organization, disposal_type: dt)
+
+    assert_not d.danger?
+  end
+
+  test "A disposal with not danger cer is legalized when approved" do
+    cer_code = FactoryBot.create(:cer_code, danger: true)
+    dt = FactoryBot.create(:disposal_type, cer_code: cer_code)
+    d = FactoryBot.create(:disposal, organization: dt.organization, disposal_type: dt)
+
+    assert d.approve!
+    assert d.approved_at
+    assert d.legalized_at
+  end
+
+  test "A disposal with danger cer is not legalized when approved" do
+    cer_code = FactoryBot.create(:cer_code, danger: false)
+    dt = FactoryBot.create(:disposal_type, cer_code: cer_code)
+    d = FactoryBot.create(:disposal, organization: dt.organization, disposal_type: dt)
+
+    assert d.approve!
+    assert d.approved_at
+    assert_not d.legalized_at
   end
 end
