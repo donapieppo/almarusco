c53f3494bd8885bd693b84e24d7b003627d21eee
Pietro <donapieppo@yahoo.it>
2023-03-24 11:01:09 +0100

    refactor

--- app/components/legal_download/line_component.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class LegalDownload::LineComponent < ViewComponent::Base
-  def initialize(legal_download)
-    @legal_download = legal_download
-  end
-end
--- app/components/picking_document/show_component.rb
+++ app/components/picking_document/show_component.rb
@@ -7,19 +7,19 @@ class PickingDocument::ShowComponent < ViewComponent::Base
     @picking = @picking_document.picking
     @disposal_type = @picking_document.disposal_type
     # just a cache usually
-    @disposals = disposals || @picking.disposals.where(disposal_type: @disposal_type) 
+    @disposals = disposals || @picking.disposals.where(disposal_type: @disposal_type)
 
-    @expected_kgs = 0.0 
+    @expected_kgs = 0.0
     @expected_volume = 0
 
     disposals.each do |disposal|
       (@expected_kgs += disposal.kgs) if disposal.kgs
-      (@expected_volume += disposal.volume_tot) if disposal.volume 
+      (@expected_volume += disposal.volume_tot) if disposal.volume
     end
 
     @all_ok = @picking_document && @picking_document.kgs && @picking_document.volume &&
-              (@picking_document.kgs - @expected_kgs).abs < 1 &&
-              @picking_document.volume == @expected_volume
+      (@picking_document.kgs - @expected_kgs).abs < 1 &&
+      @picking_document.volume == @expected_volume
 
     @picking_document_policy = PickingDocumentPolicy.new(@current_user, @picking_document)
   end
