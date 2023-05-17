20a7e8e2b28ff3505ea27e4a32bc574430f2fa1d
Pietro <donapieppo@yahoo.it>
2023-05-17 12:26:28 +0200

    hidden && containers

--- doc/changes.sql
+++ doc/changes.sql
@@ -1,3 +1,33 @@
+alter table disposal_types add column `hidden` bool not null default 0 after `separable`;
+
+create table containers (
+        `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+        `name` varchar(255),
+        `liters` int(2),
+        `notes` text,
+        PRIMARY KEY (`id`)
+);
+
+create table containers_disposal_types (
+        `container_id` int(10) unsigned NOT NULL,
+        `disposal_type_id` int(10) unsigned NOT NULL,
+        FOREIGN KEY (container_id) REFERENCES containers (id),
+        FOREIGN KEY (disposal_type_id) REFERENCES disposal_types(id)
+);
+
+insert into containers values (0, 'clinipack', 60);
+insert into containers values (0, 'tanica', 5);
+insert into containers values (0, 'tanica', 10);
+insert into containers values (0, 'tanica', 20);
+insert into containers values (0, 'polietilene', 30);
+insert into containers values (0, 'fusto', 30);
+insert into containers values (0, 'fusto', 40);
+insert into containers values (0, 'polietilene', 60);
+insert into containers values (0, 'fusto', 60);
+insert into containers values (0, 'tanica', 120);
+insert into containers values (0, 'big bag', 500);
+
+
 -- alter table organizations add column `next_local_id` int(10) default 1 after `description`;
 -- alter table disposals add column `local_id` int(10) after `disposal_type_id`;
 -- alter table disposals add KEY `k_local_id` (`local_id`);
