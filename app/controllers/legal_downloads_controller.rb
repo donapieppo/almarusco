abe9e4d5e425d14fb635a87143b68255dee1dbb1
Pietro <donapieppo@yahoo.it>
2024-01-17 14:47:41 +0100

    before_action

--- app/controllers/legal_downloads_controller.rb
+++ app/controllers/legal_downloads_controller.rb
@@ -1,18 +1,18 @@
 class LegalDownloadsController < LegalRecordsController
   before_action :set_picking_document_and_disposal_type, only: [:new, :create]
-  before_action :set_legal_download_and_check_permission, only: [:show, :edit, :update, :delete]
+  before_action :set_legal_download_and_check_permission, only: [:show, :edit, :update, :destroy]
 
   def new
-    @legal_download = current_organization.legal_downloads.new(picking_document: @picking_document, 
-                                                               disposal_type: @disposal_type, 
+    @legal_download = current_organization.legal_downloads.new(picking_document: @picking_document,
+                                                               disposal_type: @disposal_type,
                                                                date: Date.today)
     authorize @legal_download
   end
 
   def create
     @legal_download = current_organization.legal_downloads.new(picking_document: @picking_document,
-                                                               disposal_type: @disposal_type, 
-                                                               number: params[:legal_download][:number], 
+                                                               disposal_type: @disposal_type,
+                                                               number: params[:legal_download][:number],
                                                                date: params[:legal_download][:date])
     authorize @legal_download
     if @legal_download.save
@@ -22,7 +22,7 @@ class LegalDownloadsController < LegalRecordsController
     end
   end
 
-  def show 
+  def show
   end
 
   def edit
@@ -30,8 +30,7 @@ class LegalDownloadsController < LegalRecordsController
   end
 
   def update
-    if @legal_download.update(number: params[:legal_download][:number],
-                              date: params[:legal_download][:date])
+    if @legal_download.update(number: params[:legal_download][:number], date: params[:legal_download][:date])
       redirect_to @legal_download.picking_document.picking, notice: "Registrazione numero #{@legal_download.number} aggiornata correttamente."
     else
       @disposal_type = @legal_download.disposal_type
