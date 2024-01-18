83789e345fa42b2f0ad20524eba09086d2232311
Pietro <donapieppo@yahoo.it>
2024-01-18 09:54:53 +0100

    after_action moved in dm_unibo_common

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < DmUniboCommon::ApplicationController
   include DmUniboCommon::Controllers::Helpers
 
   before_action :set_current_user, :update_authorization, :set_current_organization, :log_current_user, :force_sso_user, :after_current_user_and_organization, :set_locale
-  after_action :verify_authorized, except: [:who_impersonate, :impersonate, :stop_impersonating]
 
   def after_current_user_and_organization
     if !current_user || !current_organization || !OrganizationPolicy.new(current_user, current_organization).dispose?
