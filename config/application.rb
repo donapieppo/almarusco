9d448e1cfc8b0a327e0567626cdce118d5d93793
Pietro <donapieppo@yahoo.it>
2024-05-16 09:32:15 +0200

    application

---
+++ config/application.rb
@@ -0,0 +1,28 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Almarusco
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.1
+
+    config.time_zone = "Rome"
+    config.i18n.default_locale = :it
+
+    config.authlevels = {
+      read: 10,
+      operate: 20, # operatori
+      dispose: 30, # produttori
+      manage: 40,  # delegati
+      admin: 60    # responsabili ul
+    }
+
+    config.action_mailer.default_url_options = {protocol: "https"}
+    config.dm_unibo_common = ActiveSupport::HashWithIndifferentAccess.new config_for(:dm_unibo_common)
+  end
+end
