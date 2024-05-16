2e6db8b8a8ec767461a9bd5ee3cf18a4c6adda8b
Pietro <donapieppo@yahoo.it>
2024-05-16 09:02:10 +0200

    mud 2023

--- app/controllers/mud_controller.rb
+++ app/controllers/mud_controller.rb
@@ -1,10 +1,9 @@
 class MudController < ApplicationController
   def show
     authorize :mud
-    @year = 2022
+    @year = 2023
     @mud = Mud.new(current_organization, year: @year)
     @summary = @mud.summary
     @remainders = @mud.remainders
   end
 end
-
