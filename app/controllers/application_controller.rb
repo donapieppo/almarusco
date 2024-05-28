61c93ecd223c8f184d4242b5f9a76aceb7679240
Pietro <donapieppo@yahoo.it>
2024-05-28 11:34:03 +0200

    permissions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < DmUniboCommon::ApplicationController
   include DmUniboCommon::Controllers::Helpers
 
-  before_action :set_current_user, :update_authorization, :set_current_organization, :log_current_user, :force_sso_user, :after_current_user_and_organization, :set_locale
+  before_action :set_current_user,
+    :update_authorization,
+    :set_current_organization,
+    :log_current_user,
+    :force_sso_user,
+    :after_current_user_and_organization,
+    :set_locale
 
   def after_current_user_and_organization
     if !current_user || !current_organization || !OrganizationPolicy.new(current_user, current_organization).dispose?
       skip_authorization
-      redirect_to dm_unibo_common.no_access_path and return
+      redirect_to home_path, alert: "Non hai accesso alla risorsa."
     end
   end
 
