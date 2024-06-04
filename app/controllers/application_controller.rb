109dc8002940af935818d3ce6c8d61e4b602b8a0
Pietro <donapieppo@yahoo.it>
2024-06-04 09:17:45 +0200

    before_action ordering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,16 @@ class ApplicationController < DmUniboCommon::ApplicationController
 
   before_action :set_current_user,
     :update_authorization,
-    :set_current_organization,
-    :log_current_user,
     :force_sso_user,
+    :log_current_user,
+    :set_current_organization,
     :after_current_user_and_organization,
     :set_locale
 
   def after_current_user_and_organization
     if !current_user || !current_organization || !OrganizationPolicy.new(current_user, current_organization).dispose?
       skip_authorization
-      redirect_to home_path, alert: "Non hai accesso alla risorsa."
+      redirect_to home_path, alert: "Non hai accesso alla risorsa richiesta."
     end
   end
 
