0b50c8a92f1a3d77db82563b4d58828bc508b50e
Pietro Donatini <pietro.donatini@unibo.it>
2024-06-12 10:57:40 +0200

    current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,15 @@ class ApplicationController < DmUniboCommon::ApplicationController
   before_action :set_current_user,
     :update_authorization,
     :force_sso_user,
-    :log_current_user,
     :set_current_organization,
+    :log_current_user,
     :after_current_user_and_organization,
     :set_locale
 
   def after_current_user_and_organization
     if !current_user || !current_organization || !OrganizationPolicy.new(current_user, current_organization).dispose?
       skip_authorization
-      redirect_to home_path, alert: "Non hai accesso alla risorsa richiesta."
+      redirect_to home_path(__org__: nil), alert: "Non hai accesso alla risorsa richiesta."
     end
   end
 
