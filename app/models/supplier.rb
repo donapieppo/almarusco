0045c2e38d76968a45aa73f9ed5e79119c4cd99b
Pietro Donatini <pietro.donatini@unibo.it>
2024-08-08 15:17:57 +0200

    rubocop

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -4,9 +4,11 @@ class Supplier < ApplicationRecord
   has_many :contracts
   has_many :cer_codes, through: :contracts
 
-  validates :name, format: {with: /\w/, message: "Il nome del destinatario non mi sembra corretto."}
-  validates :pi, format: {with: /\A[0-9a-zA-Z]{11}\z/, message: "La partita iva deve contenere 11 caratteri."},
-                 uniqueness: {message: "Esiste già un destinatario con la stessa partita iva", case_sensitive: false}
+  validates :name,
+    format: {with: /\w/, message: "Il nome del destinatario non mi sembra corretto."}
+  validates :pi,
+    format: {with: /\A[0-9a-zA-Z]{11}\z/, message: "La partita iva deve contenere 11 caratteri."},
+    uniqueness: {message: "Esiste già un destinatario con la stessa partita iva", case_sensitive: false}
 
   def to_s
     self.name
