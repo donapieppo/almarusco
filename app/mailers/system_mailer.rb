36041c055fb75e0d4a7806b9f70903abfee184ee
Pietro Donatini <pietro.donatini@unibo.it>
2024-08-09 13:01:43 +0200

    rubocop

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -5,17 +5,21 @@ class SystemMailer < ApplicationMailer
     @expiration_date = permission.expiry
     @producer = User.find(permission.producer_id)
 
-    mail(to:      @user.upn, 
-         bcc:     'pietro.donatini@unibo.it', 
-         subject: "Avviso scadenza accesso applicativo AlmaRusco.")
+    mail(
+      to: @user.upn,
+      bcc: "pietro.donatini@unibo.it",
+      subject: "Avviso scadenza accesso applicativo AlmaRusco."
+    )
   end
 
   def notify_approved_disposals(user, disposals)
-    @user      = user
+    @user = user
     @disposals = disposals
 
-    mail(to:      @user.upn, 
-         bcc:     'pietro.donatini@unibo.it', 
-         subject: "Conferimento rifiuti.")
+    mail(
+      to: @user.upn,
+      bcc: "pietro.donatini@unibo.it",
+      subject: "Conferimento rifiuti."
+    )
   end
 end
