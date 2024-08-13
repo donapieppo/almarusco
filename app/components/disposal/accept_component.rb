8ddfd78e84a658e3cbdc166e322bc92395bdaa86
Pietro Donatini <pietro.donatini@unibo.it>
2024-08-13 12:34:25 +0200

    rubocop

--- app/components/disposal/accept_component.rb
+++ app/components/disposal/accept_component.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Disposal::AcceptComponent < ViewComponent::Base
   def initialize(current_user, disposal)
     @current_user = current_user
@@ -10,8 +8,6 @@ class Disposal::AcceptComponent < ViewComponent::Base
 
   # FIXME with DisposalPolicy#approve? unapprove?
   def render?
-    (! @disposal.assigned?) && OrganizationPolicy.new(@current_user, @disposal.organization).manage?
+    (!@disposal.assigned?) && OrganizationPolicy.new(@current_user, @disposal.organization).manage?
   end
 end
-
-
