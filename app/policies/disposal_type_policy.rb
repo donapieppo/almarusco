62f5135242911e3ecbb0119ad0b760c8320a6c12
Pietro Donatini <pietro.donatini@unibo.it>
2024-08-27 09:32:06 +0200

    show and destroy

--- app/policies/disposal_type_policy.rb
+++ app/policies/disposal_type_policy.rb
@@ -3,11 +3,19 @@ class DisposalTypePolicy < ApplicationPolicy
     @user
   end
 
+  def show?
+    @user && current_organization_manager?
+  end
+
   def create?
     @user && current_organization_manager?
   end
 
   def update?
     @user && OrganizationPolicy.new(@user, @record.organization_id).manage?
+  end
+
+  def destroy?
+    @user && OrganizationPolicy.new(@user, @record.organization_id).manage? && @record.disposals.empty?
   end
 end
