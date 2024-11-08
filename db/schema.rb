a4338f3ea966eda7067305e036271e3111b9c068
Pietro Donatini <pietro.donatini@unibo.it>
2024-11-08 12:27:38 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_153320) do
     t.index ["name"], name: "name"
   end
 
+  create_table "adrs_component_details", id: false, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.integer "component_detail_id", null: false, unsigned: true
+    t.integer "adr_id", null: false, unsigned: true
+    t.index ["adr_id"], name: "fk_component_details_adrs_adr"
+    t.index ["component_detail_id"], name: "fk_component_details_adrs_component"
+  end
+
   create_table "adrs_disposal_types", id: false, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.integer "adr_id", unsigned: true
     t.integer "disposal_type_id", unsigned: true
@@ -44,6 +51,14 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_153320) do
     t.integer "supplier_id", null: false, unsigned: true
     t.index ["cer_code_id"], name: "fk_ccs_cer_code"
     t.index ["supplier_id"], name: "fk_ccs_supplier"
+  end
+
+  create_table "compliances", id: { type: :integer, unsigned: true, default: nil }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.integer "organization_id", unsigned: true
+    t.text "name"
+    t.text "description"
+    t.integer "year", unsigned: true
+    t.text "url"
   end
 
   create_table "component_details", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -96,10 +111,19 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_153320) do
     t.integer "organization_id", null: false, unsigned: true
     t.integer "user_id", null: false, unsigned: true
     t.text "name"
-    t.integer "lab_id", unsigned: true
-    t.index ["lab_id"], name: "fk_disposal_descriptions_labs"
+    t.text "department"
+    t.text "place"
+    t.string "chief"
+    t.text "lab"
     t.index ["organization_id"], name: "fk_disposal_descriptions_organizations"
     t.index ["user_id"], name: "fk_disposal_descriptions_users"
+  end
+
+  create_table "disposal_descriptions_pictograms", id: false, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.integer "disposal_description_id", unsigned: true
+    t.integer "pictogram_id", unsigned: true
+    t.index ["disposal_description_id"], name: "fk_ddp_dt"
+    t.index ["pictogram_id"], name: "fk_ddp_picto"
   end
 
   create_table "disposal_types", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -111,7 +135,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_153320) do
     t.boolean "hidden", default: false, null: false
     t.boolean "legalizable", default: false, null: false
     t.text "notes"
+    t.integer "compliance_id", unsigned: true
+    t.text "compliance_alert"
+    t.timestamp "created_at"
     t.index ["cer_code_id"], name: "fk_disposal_types_cer"
+    t.index ["compliance_id"], name: "fw_compliances_disposal_types"
     t.index ["organization_id"], name: "fk_disposal_types_organization"
     t.index ["un_code_id"], name: "fk_disposal_types_un"
   end
@@ -268,6 +296,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_153320) do
     t.index ["upn"], name: "index_dsacaches_on_upn", unique: true
   end
 
+  add_foreign_key "adrs_component_details", "adrs", name: "fk_component_details_adrs_adr"
+  add_foreign_key "adrs_component_details", "component_details", name: "fk_component_details_adrs_component"
   add_foreign_key "adrs_disposal_types", "adrs", name: "fk_adrdt_adr"
   add_foreign_key "adrs_disposal_types", "disposal_types", name: "fk_adrdt_dt"
   add_foreign_key "buildings", "organizations", name: "buildings_ibfk_1"
@@ -283,10 +313,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_153320) do
   add_foreign_key "containers_disposal_types", "disposal_types", name: "containers_disposal_types_ibfk_2"
   add_foreign_key "contracts", "cer_codes", name: "contracts_ibfk_2"
   add_foreign_key "contracts", "suppliers", name: "contracts_ibfk_1"
-  add_foreign_key "disposal_descriptions", "labs", name: "fk_disposal_descriptions_labs"
   add_foreign_key "disposal_descriptions", "organizations", name: "fk_disposal_descriptions_organizations"
   add_foreign_key "disposal_descriptions", "users", name: "fk_disposal_descriptions_users"
+  add_foreign_key "disposal_descriptions_pictograms", "disposal_types", column: "disposal_description_id", name: "fk_ddp_dt"
+  add_foreign_key "disposal_descriptions_pictograms", "pictograms", name: "fk_ddp_picto"
   add_foreign_key "disposal_types", "cer_codes", name: "fk_disposal_types_cer"
+  add_foreign_key "disposal_types", "compliances", name: "fw_compliances_disposal_types"
   add_foreign_key "disposal_types", "organizations", name: "fk_disposal_types_organization"
   add_foreign_key "disposal_types", "un_codes", name: "fk_disposal_types_un"
   add_foreign_key "disposal_types_hp_codes", "disposal_types", name: "fk_dthc_disposal"
