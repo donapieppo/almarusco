60a809b7e94b8c7d595269afba2c15a3c590f4c7
Pietro Donatini <pietro.donatini@unibo.it>
2024-11-08 11:36:51 +0100

    compliance

--- app/components/disposal_type/card_component.rb
+++ app/components/disposal_type/card_component.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class DisposalType::CardComponent < ViewComponent::Base
-  def initialize(current_user, disposal_type, editable: true, with_details: true)
+  def initialize(current_user, disposal_type, editable: true, with_details: true, with_links: true)
     @current_user = current_user
     @disposal_type = disposal_type
     @with_details = with_details
+    @with_links = with_links
 
     @editable = editable
     @policy = DisposalTypePolicy.new(@current_user, @disposal_type)
