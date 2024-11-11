4acc5e9a0f2701040683fa0b3244a2330b1fc4bf
Pietro Donatini <pietro.donatini@unibo.it>
2024-11-11 13:13:00 +0100

    initialize

---
+++ config/initializers/almarusco.rb
@@ -0,0 +1,11 @@
+# upn di amministratori del programma (Array di upn)
+NUTER = ["nome.cognome@example.com"]
+
+module Almarusco
+  class Application < Rails::Application
+    config.home_description = "
+<p>Ogni rifiuto gestito attraverso l’applicazione è registrato separatamente in funzione della sua classificazione EER (Elenco Europeo dei Rifiuti), dello stato fisico, della eventuale pericolosità HP e ADR e dei quantitativi prodotti.</p>
+<p>L'applicazione consente: al produttore del rifiuto di stampare le etichette dei contenitori e prenotare la consegna del rifiuto presso il Deposito Temporaneo prima della Raccolta, mentre ai Responsabili di Unità  Locale /Delegati alle Operazioni di tracciare puntualmente i rifiuti in Deposito, semplificandone e velocizzandone le operazioni di gestione, diminuendo al contempo il rischio di errore.</p>
+<p>La puntuale tracciatura facilita inoltre la rendicontazione dei rifiuti prodotti, sia ai fini della gestione interna che a quelli legati agli obblighi di legge e alla rendicontazione ambientale.</p>".html_safe
+  end
+end
