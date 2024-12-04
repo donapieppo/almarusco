68c51fe650caaf5ba0f286f9cdc6c15604f33eec
Pietro Donatini <pietro.donatini@unibo.it>
2024-12-04 15:17:11 +0100

    policy

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -3,11 +3,11 @@ class ApplicationPolicy < DmUniboCommon::ApplicationPolicy
     @user && OrganizationPolicy.new(@user, o).dispose?
   end
 
-  def current_organization_manager?
-    @user.nuter? || super
-  end
-
   def current_organization_disposer?
     organization_disposer?(@user.current_organization)
+  end
+
+  def current_organization_manager?
+    @user && (organization_manager?(@user.current_organization) || @user.nuter?)
   end
 end
