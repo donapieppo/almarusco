c17c38b00ba8dbf6440d56cf1429609ee0f5e591
Pietro Donatini <pietro.donatini@unibo.it>
2024-12-09 13:25:33 +0100

    dependent

--- app/models/compliance.rb
+++ app/models/compliance.rb
@@ -1,7 +1,8 @@
 # https://intranet.unibo.it/SpaziIct/Web3/Pagine/GestioneRifiutiPericolosiProcedura.aspx
 class Compliance < ApplicationRecord
+  # omologa di ateneo id organization_id=nil
   belongs_to :organization, optional: true
-  has_many :disposal_types
+  has_many :disposal_types, dependent: :nullify
 
   has_one_attached :document
 
@@ -15,6 +16,8 @@ class Compliance < ApplicationRecord
     !organization_id
   end
 
+  # Compliance.new(common: true) means it belongs (can be used)
+  # to all all organizations
   def common=(x)
     if x
       self.organization_id = nil
