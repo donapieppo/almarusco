4619776de13917fab0c79d195447e7c65d2449ac
Pietro Donatini <pietro.donatini@unibo.it>
2024-12-13 10:07:57 +0100

    lograge

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,9 +1,9 @@
 Rails.application.configure do
   config.lograge.enabled = true
-  config.lograge.formatter = Lograge::Formatters::Logstash.new
-  config.lograge.ignore_actions = ["Rails::HealthController#show"]
+  # config.lograge.formatter = Lograge::Formatters::Logstash.new
+  config.lograge.formatter = Lograge::Formatters::Json.new
   config.lograge.custom_payload do |controller|
-    if !controller.is_a?(Rails::HealthController)
+    if ! controller.is_a?(Rails::HealthController)
       res = {
         host: controller.request.host,
         current_user: controller.current_user&.upn,
@@ -12,8 +12,8 @@ Rails.application.configure do
       if controller.current_user != controller.true_user
         res[:true_user] = controller.true_user&.upn
       end
+      res
     end
-    res
   end
   #     if current_user != true_user
   #       logger.info("#{true_user.upn} IMPERSONATING #{current_user.upn} on #{current_organization&.code}")
