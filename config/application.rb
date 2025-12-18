81c3eb5fe8892c8323fa561504451c729592cb4f
Pietro Donatini <pietro.donatini@unibo.it>
2025-12-18 10:44:09 +0100

    rails8

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Almarusco
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 8.1
 
     config.hosts += ENV.fetch("ALLOWED_HOSTS", "").split(",")
 
