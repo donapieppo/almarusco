c14a61cfb3f240fe61077c816e59af586ea39208
Pietro Donatini <pietro.donatini@unibo.it>
2025-12-22 11:10:15 +0100

    rails8

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2023_11_23_153320) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_16_130813) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
+    t.string "content_type"
     t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -40,8 +40,8 @@ ActiveRecord::Schema[7.2].define(version: 2023_11_23_153320) do
   end
 
   create_table "adrs", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.string "name"
     t.text "description"
+    t.string "name"
     t.index ["name"], name: "name"
   end
 
@@ -53,17 +53,17 @@ ActiveRecord::Schema[7.2].define(version: 2023_11_23_153320) do
   end
 
   create_table "buildings", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.integer "organization_id", unsigned: true
-    t.string "name", null: false
     t.text "address"
     t.text "description"
+    t.string "name", null: false
+    t.integer "organization_id", unsigned: true
     t.index ["organization_id"], name: "organization_id"
   end
 
   create_table "cer_codes", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.string "name", limit: 100
-    t.text "description"
     t.boolean "danger", default: false
+    t.text "description"
+    t.string "name", limit: 100
     t.index ["name"], name: "name"
   end
 
@@ -75,17 +75,17 @@ ActiveRecord::Schema[7.2].define(version: 2023_11_23_153320) do
   end
 
   create_table "compliances", id: { type: :integer, unsigned: true, default: nil }, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.integer "organization_id", unsigned: true
-    t.text "name"
     t.text "description"
-    t.integer "year", unsigned: true
+    t.text "name"
+    t.integer "organization_id", unsigned: true
     t.text "url"
+    t.integer "year", unsigned: true
   end
 
   create_table "containers", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.column "name", "enum('tanica','bidone polietilene','clinipack','fusto','big bag')"
-    t.integer "volume"
     t.text "notes"
+    t.integer "volume"
   end
 
   create_table "containers_disposal_types", id: false, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -96,27 +96,27 @@ ActiveRecord::Schema[7.2].define(version: 2023_11_23_153320) do
   end
 
   create_table "contracts", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.integer "supplier_id", null: false, unsigned: true
     t.integer "cer_code_id", null: false, unsigned: true
+    t.date "end_date"
     t.decimal "price", precision: 6, scale: 2, default: "0.0", unsigned: true
     t.date "start_date"
-    t.date "end_date"
+    t.integer "supplier_id", null: false, unsigned: true
     t.index ["cer_code_id"], name: "cer_code_id"
     t.index ["supplier_id"], name: "supplier_id"
   end
 
   create_table "disposal_types", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.integer "organization_id", unsigned: true
-    t.integer "compliance_id", unsigned: true
     t.integer "cer_code_id", unsigned: true
-    t.integer "un_code_id", unsigned: true
-    t.column "physical_state", "enum('liq','sp','snp')"
-    t.boolean "separable", default: false, null: false
+    t.text "compliance_alert"
+    t.integer "compliance_id", unsigned: true
+    t.timestamp "created_at", default: -> { "current_timestamp() ON UPDATE current_timestamp()" }, null: false
     t.boolean "hidden", default: false, null: false
     t.boolean "legalizable", default: false, null: false
     t.text "notes"
-    t.text "compliance_alert"
-    t.timestamp "created_at", default: -> { "current_timestamp() ON UPDATE current_timestamp()" }, null: false
+    t.integer "organization_id", unsigned: true
+    t.column "physical_state", "enum('liq','sp','snp')"
+    t.boolean "separable", default: false, null: false
+    t.integer "un_code_id", unsigned: true
     t.index ["cer_code_id"], name: "fk_disposal_types_cer"
     t.index ["compliance_id"], name: "fw_compliances_disposal_types"
     t.index ["organization_id"], name: "fk_disposal_types_organization"
@@ -138,24 +138,24 @@ ActiveRecord::Schema[7.2].define(version: 2023_11_23_153320) do
   end
 
   create_table "disposals", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.integer "user_id", unsigned: true
+    t.date "approved_at"
+    t.date "completed_at"
+    t.integer "container_id", unsigned: true
+    t.date "created_at"
+    t.date "delivered_at"
+    t.integer "disposal_type_id", unsigned: true
+    t.decimal "kgs", precision: 10, scale: 3, default: "0.0"
     t.integer "lab_id", unsigned: true
+    t.integer "legal_record_id", unsigned: true
+    t.date "legalized_at"
+    t.boolean "multiple_users", default: false
+    t.text "notes"
+    t.integer "organization_id", unsigned: true
     t.integer "picking_id", unsigned: true
     t.integer "producer_id", unsigned: true
-    t.boolean "multiple_users", default: false
-    t.integer "organization_id", unsigned: true
-    t.integer "disposal_type_id", unsigned: true
-    t.integer "legal_record_id", unsigned: true
-    t.text "notes"
-    t.decimal "kgs", precision: 10, scale: 3, default: "0.0"
-    t.integer "volume"
-    t.integer "container_id", unsigned: true
     t.integer "units", limit: 2, default: 1
-    t.date "created_at"
-    t.date "approved_at"
-    t.date "legalized_at"
-    t.date "delivered_at"
-    t.date "completed_at"
+    t.integer "user_id", unsigned: true
+    t.integer "volume"
     t.index ["container_id"], name: "container_id"
     t.index ["disposal_type_id"], name: "fk_disposals_disposal_type"
     t.index ["lab_id"], name: "fk_disposals_labs"
@@ -167,35 +167,35 @@ ActiveRecord::Schema[7.2].define(version: 2023_11_23_153320) do
   end
 
   create_table "hazards", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.column "category", "enum('Fisico','Salute','Ambiente')", null: false
     t.string "code", limit: 20, null: false
     t.text "phrase", null: false
-    t.column "category", "enum('Fisico','Salute','Ambiente')", null: false
   end
 
   create_table "hp_codes", id: { type: :integer, unsigned: true, default: nil }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.string "name"
     t.text "description"
+    t.string "name"
     t.index ["name"], name: "name"
   end
 
   create_table "labs", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.integer "building_id", unsigned: true
-    t.integer "organization_id", unsigned: true
     t.string "name"
+    t.integer "organization_id", unsigned: true
     t.index ["building_id"], name: "building_id"
     t.index ["organization_id"], name: "fk_labs_organizations"
   end
 
   create_table "legal_records", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.column "type", "enum('LegalUpload','LegalDownload')"
-    t.integer "organization_id", null: false, unsigned: true
-    t.integer "disposal_type_id", null: false, unsigned: true
-    t.integer "picking_document_id", unsigned: true
-    t.integer "year", null: false, unsigned: true
-    t.date "date"
-    t.integer "number", null: false, unsigned: true
     t.date "created_at"
+    t.date "date"
+    t.integer "disposal_type_id", null: false, unsigned: true
+    t.integer "number", null: false, unsigned: true
+    t.integer "organization_id", null: false, unsigned: true
+    t.integer "picking_document_id", unsigned: true
+    t.column "type", "enum('LegalUpload','LegalDownload')"
     t.date "updated_at"
+    t.integer "year", null: false, unsigned: true
     t.index ["disposal_type_id"], name: "disposal_type_id"
     t.index ["number"], name: "k_number"
     t.index ["organization_id", "year", "number"], name: "organization_id", unique: true
@@ -203,33 +203,33 @@ ActiveRecord::Schema[7.2].define(version: 2023_11_23_153320) do
   end
 
   create_table "organizations", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.string "code"
-    t.string "name"
-    t.string "description"
     t.string "adminmail", limit: 200
-    t.datetime "updated_at", precision: nil
+    t.string "code"
     t.datetime "created_at", precision: nil
+    t.string "description"
+    t.string "name"
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "permissions", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.integer "user_id", unsigned: true
-    t.integer "organization_id", unsigned: true
-    t.string "network", limit: 20
     t.integer "authlevel", unsigned: true
-    t.datetime "updated_at", precision: nil
     t.datetime "created_at", precision: nil
-    t.integer "producer_id", unsigned: true
     t.date "expiry"
+    t.string "network", limit: 20
+    t.integer "organization_id", unsigned: true
+    t.integer "producer_id", unsigned: true
+    t.datetime "updated_at", precision: nil
+    t.integer "user_id", unsigned: true
     t.index ["organization_id"], name: "fk_organization_permission"
     t.index ["producer_id"], name: "fk_permission_producer"
     t.index ["user_id"], name: "fk_user_permission"
   end
 
   create_table "picking_documents", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.integer "picking_id", null: false, unsigned: true
     t.integer "disposal_type_id", null: false, unsigned: true
-    t.string "serial_number"
     t.decimal "kgs", precision: 10, scale: 3, default: "0.0"
+    t.integer "picking_id", null: false, unsigned: true
+    t.string "serial_number"
     t.decimal "volume", precision: 10, scale: 3
     t.index ["disposal_type_id"], name: "fk_picking_document_disposal_type"
     t.index ["picking_id", "disposal_type_id"], name: "picking_id", unique: true
@@ -237,27 +237,27 @@ ActiveRecord::Schema[7.2].define(version: 2023_11_23_153320) do
   end
 
   create_table "pickings", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.integer "organization_id", null: false, unsigned: true
-    t.integer "supplier_id", null: false, unsigned: true
-    t.date "date"
     t.text "address"
+    t.date "completed_at"
     t.text "contact"
     t.datetime "created_at", precision: nil
+    t.date "date"
     t.date "delivered_at"
-    t.date "completed_at"
+    t.integer "organization_id", null: false, unsigned: true
+    t.integer "supplier_id", null: false, unsigned: true
     t.index ["organization_id"], name: "fk_pickings_organizations"
     t.index ["supplier_id"], name: "fk_pickings_suppliers"
   end
 
   create_table "pictograms", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.string "name"
     t.string "filename"
+    t.string "name"
   end
 
   create_table "suppliers", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.text "address"
     t.string "name"
     t.string "pi"
-    t.text "address"
   end
 
   create_table "un_codes", id: { type: :integer, unsigned: true, default: nil }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -265,13 +265,13 @@ ActiveRecord::Schema[7.2].define(version: 2023_11_23_153320) do
   end
 
   create_table "users", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.string "upn", limit: 150
+    t.string "email"
+    t.integer "employee_id", unsigned: true
     t.string "gender", limit: 1
     t.string "name", limit: 50
     t.string "surname", limit: 50
-    t.string "email"
-    t.integer "employee_id", unsigned: true
     t.timestamp "updated_at", default: -> { "current_timestamp() ON UPDATE current_timestamp()" }, null: false
+    t.string "upn", limit: 150
     t.index ["upn"], name: "index_dsacaches_on_upn", unique: true
   end
 
