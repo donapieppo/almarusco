2d616dab7273a4b044eafe0026180078b83dae40
Pietro Donatini <pietro.donatini@unibo.it>
2026-05-04 16:10:45 +0200

    small params on Disposal::BadgeComponent

--- app/components/disposal/badge_component.rb
+++ app/components/disposal/badge_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class Disposal::BadgeComponent < ViewComponent::Base
-  def initialize(current_user, disposal)
+  def initialize(current_user, disposal, small: false)
     @current_user = current_user
     @disposal = disposal
+    @small = small
 
     @badge_color = @disposal.legalized? ? 'bg-info' : 'bg-warning text-dark'
   end
