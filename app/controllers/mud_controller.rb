7d17b621e818f8bebf254b89e0069df575a169e6
Pietro Donatini <pietro.donatini@unibo.it>
2026-05-04 16:10:07 +0200

    remainders

--- app/controllers/mud_controller.rb
+++ app/controllers/mud_controller.rb
@@ -1,9 +1,10 @@
 class MudController < ApplicationController
   def show
     authorize :mud
-    @year = params[:y] ? params[:y].to_i : 2024
+    @year = params[:y] ? params[:y].to_i : 2026
     @mud = Mud.new(current_organization, @year)
     @picked_kgs = @mud.picked_kgs
     @remainders_kgs = @mud.remainders_kgs
+    @remainders = @mud.remainders
   end
 end
